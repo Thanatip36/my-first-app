bb31cba1dfbb85f72076f5e486ca78c0e1a38329
Thanatip36 <56304@yupparaj.ac.th>
2026-08-03 13:33:17 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,17 @@ import streamlit as st
 st.title("แอปพลีเคชั่นแปลง พ.ศ. เป็น ค.ศ.")
 bh_year = st.number_input("กรอก พ.ศ. ที่ต้องการแปลง", value=2569)
 ce_year = bh_year - 543
-if ce_year > 0: 
+if bh_year < 0:
+    st.warning("ปีติดลบ")
+    st.stop() 
+before = true
+if st.button("หลังพุทธศักราช"):
+    before = false
+if st.button("ก่อนปีพุทธศักราช"):
+    before = true
+if ce_year >= 0: 
+  if before:
+    bh_year = -abs(bh_year)
   st.header(f"ปี ค.ศ. {ce_year}")
 else:
   st.header(f"ปี {abs(ce_year)} ก่อนคริสตกาล ")
