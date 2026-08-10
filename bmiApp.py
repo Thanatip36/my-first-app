2721192b12c62de5b369921017011fcaa8068760
Thanatip36 <56304@yupparaj.ac.th>
2026-08-10 13:32:21 +0700

    Create bmiApp.py

---
+++ bmiApp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.martdown("# :red[🏋️ คำนวนค่าดัชนีมาลกาย BMI]")
+st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื่องต้น")
+
+weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):" , min_value = 1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value  = 1.0)
+
+if st.button("คำนวณค่า BMI 🎯"):
+  height_m = height_cm / 100
+  bmi = weight / (height_m ** 2)
+
+  st.write("---")
+  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**)
+  
+  if bmi < 18.5:
+    st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+  elif 18.5 <= bmi < 23.0:
+      st.success("🌈 คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+  elif 23.0 <= bmi < 25.0:
+      st.info("💡 คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
+  else:
+      st.error("⚠️ คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+  
+  st.divider()
+  st.write("นายธนาธิป พัชรวงค์ เลขที่ 5 ม.4/3")
