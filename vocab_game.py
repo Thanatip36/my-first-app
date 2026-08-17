4a7c265b75fcab5528ab1228376d361cdf3c7051
Thanatip36 <56304@yupparaj.ac.th>
2026-08-17 13:19:17 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายธนาธิป พัชรวงค์ เลขที่ 43 ม.4/3")
 
 
