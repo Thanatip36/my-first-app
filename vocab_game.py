aaa9459229f9504d4b6a0af8ba5b2142195d62da
Thanatip36 <56304@yupparaj.ac.th>
2026-09-14 13:36:21 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -1,6 +1,16 @@
 import time
 import streamlit as st
+from PIL import Image
 
+
+image_path = "Thumbs_up_Emoji.png"  
+
+try:
+    image = Image.open(image_path)
+    st.image(image, caption="My App Logo", use_container_width=True)
+) except FileNotFoundError:
+    st.error("Image file not found. Make sure it is pushed to GitHub.")
+    
 st.title("⏱️ เกมเติมศัพท์จับเวลา")
 
 answers = ["Imposter", "ก้มโดนตบ", "พระบิดา", "67", "คาเนกิ"]
